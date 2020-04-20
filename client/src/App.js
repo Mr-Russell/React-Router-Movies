877adc3cab88d74c69b2a0269ef7acc3f2b4e2d4
Brian Russell <bruss@brians-mbp.lan>
2020-04-20 18:03:52 -0400

    Completed MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
-
+import {Route, Switch} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList.js'
+import Movie from './Movies/Movie.js'
+
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
+    console.log(movie)
   };
-
+   // console.log(savedList)
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Switch>
+          <Route path='/movies/:id'>
+            <Movie />
+          </Route>
+
+          <Route path='/'>
+            <MovieList />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 };
